6613c9ceacde7d3d750c579ffbd5bc1615a1382c
fanischam <fchamalidis@yahoo.com>
2021-12-15 18:22:25 +0200

    Changed some styles

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -4,7 +4,10 @@ import "./Logo.css";
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0 br2 shadow-2" style={{ height: 100, width: 100 }}>
+    <div
+      className="logo-container ma4 mt0 br2 shadow-2"
+      style={{ height: 100, width: 100 }}
+    >
       <div className="pa3">
         <img style={{ paddingTop: "5px" }} src={brain} alt="brain" />
       </div>
